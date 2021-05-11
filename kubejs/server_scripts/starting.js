b7f28cca617ccaa914fbe5da7edf91e34b25818f
mo_shark <medoelzarkalt@gmail.com>
2021-05-11 18:12:08 -0400

    Removed Antique Atlas & Soul Compass from starting items
    - Removed Antique Atlas mod
    - Added Xaero's World Map
    - Added Xaero's Minimap

--- kubejs/server_scripts/starting.js
+++ kubejs/server_scripts/starting.js
@@ -5,13 +5,7 @@ events.listen('player.logged_in', function (event) {
       // Add the gamestage
       event.addGameStage('starting_items')
       // Give some items to player
-      event.player.give (Item.of('antiqueatlas:antique_atlas', {"quark:RuneAttached":1,Enchantments:[{lvl:1,id:"soulbound:soulbound"}],"quark:RuneColor":{id:"quark:black_rune",Count:1}}))
       event.player.give (Item.of('enigmaticlegacy:enigmatic_amulet', {"quark:RuneAttached":1,Enchantments:[{lvl:1,id:"soulbound:soulbound"}],"quark:RuneColor":{id:"quark:black_rune",Count:1}}))
       event.player.give (Item.of('enigmaticlegacy:cursed_ring', {"quark:RuneAttached":1,Enchantments:[{lvl:1,id:"soulbound:soulbound"}],"quark:RuneColor":{id:"quark:black_rune",Count:1}}))
-      event.player.give (Item.of('quark:soul_compass', {"quark:RuneAttached":1,Enchantments:[{lvl:1,id:"soulbound:soulbound"}],"quark:RuneColor":{id:"quark:black_rune",Count:1}}))
     }
   })
-
-
-//give @p quark:soul_compass{Enchantments:[{id: 'soulbound:soulbound',lvl:1}]} 1
-//give @p quark:soul_compass{"quark:RuneAttached":1,Enchantments:[{id: 'soulbound:soulbound',lvl:1}],"quark:RuneColor":{id:"quark:black_rune",Count:1}} 1
