06af896033e722ea598a56ec34d11fe55f631f05
mo_shark <medoelzarkalt@gmail.com>
2021-12-22 19:04:51 -0500

    data-driven recipe rework

--- kubejs/server_scripts/shaped.js
+++ kubejs/server_scripts/shaped.js
@@ -230,198 +230,113 @@ onEvent('recipes', (event) => {
             key: {
                 A: 'minecraft:stick'
             },
+            id: 'momo:ladders'
         }
     ]
     recipes.forEach((recipe) => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
     });
 
+    /*
+    event.custom({
+        type: ''
+    })
+    */
+    
     //Greater Eye of Ender recipe
-    event.custom(
-        {
-            "type": "farmersdelight:cooking",
-            "ingredients": [
-                {
-                    "item": "momo:lesser_ender_eye"
-                },
-                {
-                    "item": "alexsmobs:blood_sac"
-                },
-                {
-                    "item": "momo:hemolymph_pustules"
-                }
-            ],
-            "result": {
-                "item": "minecraft:ender_eye"
-            },
-            "cookingtime": 200
-        }
-    ).id("minecraft:ender_eye")
+    event.custom({
+        type: 'farmersdelight:cooking',
+        ingredients: [
+            { item: 'momo:lesser_ender_eye' },
+            { item: 'alexsmobs:blood_sac' },
+            { item: 'momo:hemolymph_pustules' },
+        ],
+        result: { item: 'minecraft:ender_eye' },
+        cookingtime: 200
+    }).id('minecraft:ender_eye')
 
-
+    
     //Pocket recipes
-    event.custom(
-        {
-            "type": "improvedbackpacks:sewing",
-            "spools_count": 1,
-            "first": {
-                "item": "improvedbackpacks:tanned_leather"
-            },
-            "first_count": 3,
-            "second": {
-                "tag": "forge:shulker_boxes"
-            },
-            "result": "improvedbackpacks:large_pocket"
-        }
-    ).id('improvedbackpacks:large_pocket')
-    event.custom(
-        {
-            "type": "improvedbackpacks:sewing",
-            "spools_count": 1,
-            "first": {
-                "item": "improvedbackpacks:tanned_leather"
-            },
-            "first_count": 2,
-            "second": {
-                "tag": "forge:ingots/netherite"
-            },
-            "result": "improvedbackpacks:medium_pocket"
-        }
-    ).id('improvedbackpacks:medium_pocket')
-    event.custom(
-        {
-            "type": "improvedbackpacks:sewing",
-            "spools_count": 1,
-            "first": {
-                "item": "improvedbackpacks:tanned_leather"
-            },
-            "second": {
-                "tag": "forge:ingots/gold"
-            },
-            "result": "improvedbackpacks:tiny_pocket"
-        }
-    ).id('improvedbackpacks:tiny_pocket')
+    //Large
+    event.custom({
+        type: 'improvedbackpacks:sewing',
+        spools_count: 3,
+        first: { item: 'improvedbackpacks:tanned_leather' },
+        first_count: 3,
+        second: { tag: 'forge:shulker_boxes' },
+        result: 'improvedbackpacks:large_pocket'
+    }).id('improvedbackpacks:large_pocket')
+    //Medium
+    event.custom({
+        type: 'improvedbackpacks:sewing',
+        spools_count: 2,
+        first: { item: 'improvedbackpacks:tanned_leather' },
+        first_count: 2,
+        second: { tag: 'forge:ingots/netherite'},
+        result: 'improvedbackpacks:medium_pocket'
+    }).id('improvedbackpacks:medium_pocket')
+    //Tiny
+    event.custom({
+        type: 'improvedbackpacks:sewing',
+        spools_count: 1,
+        first: { item: 'improvedbackpacks:tanned_leather' },
+        first_count: 1,
+        second: { tag: 'forge:ingots/gold'},
+        result: 'improvedbackpacks:tiny_pocket'
+    }).id('improvedbackpacks:tiny_pocket')
 
 
     //Crystalite Armor recipes
     //Helmet
-    event.custom(
-        {
-            "type": "betterendforge:infusion",
-            "input":
-            {
-                "item": "minecraft:netherite_helmet"
-            },
-            "output": "betterendforge:crystalite_helmet",
-            "time": 150,
-            "catalysts": [
-                {
-                    "item": "betterendforge:amber_gem",
-                    "index": 0
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 2
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 6
-                }
-            ]
-        }
-    ).id('betterendforge:')
+    event.custom({
+        type: 'betterendforge:infusion',
+        input: { item: 'minecraft:netherite_helmet' },
+        output: 'betterendforge:crystalite_helmet',
+        time: 150,
+        catalysts: [
+            { item: 'betterendforge:amber_gem', index: 0},
+            { item: 'betterendforge:crystal_shards', index: 2},
+            { item: 'betterendforge:crystal_shards', index: 6},
+        ]
+    }).id('betterendforge:crystalite_helmet')
     //Chestplate
-    event.custom(
-        {
-            "type": "betterendforge:infusion",
-            "input":
-            {
-                "item": "minecraft:netherite_chestplate"
-            },
-            "output": "betterendforge:crystalite_chestplate",
-            "time": 300,
-            "catalysts": [
-                {
-                    "item": "betterendforge:amber_gem",
-                    "index": 0
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 1
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 3
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 5
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 7
-                }
-            ]
-        }
-    ).id('betterendforge:')
+    event.custom({
+        type: 'betterendforge:infusion',
+        input: { item: 'minecraft:netherite_chestplate' },
+        output: 'betterendforge:crystalite_chestplate',
+        time: 300,
+        catalysts: [
+            { item: 'betterendforge:amber_gem', index: 0},
+            { item: 'betterendforge:crystal_shards', index: 1 },
+            { item: 'betterendforge:crystal_shards', index: 3 },
+            { item: 'betterendforge:crystal_shards', index: 5 },
+            { item: 'betterendforge:crystal_shards', index: 7 }
+        ]
+    }).id('betterendforge:crystalite_chestplate')
     //Leggings
-    event.custom(
-        {
-            "type": "betterendforge:infusion",
-            "input":
-            {
-                "item": "minecraft:netherite_leggings"
-            },
-            "output": "betterendforge:crystalite_leggings",
-            "time": 225,
-            "catalysts": [
-                {
-                    "item": "betterendforge:amber_gem",
-                    "index": 0
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 2
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 4
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 6
-                }
-            ]
-        }
-    ).id('betterendforge:')
+    event.custom({
+        type: 'betterendforge:infusion',
+        input: { item: 'minecraft:netherite_leggings' },
+        output: 'betterendforge:crystalite_leggings',
+        time: 225,
+        catalysts: [
+            { item: 'betterendforge:amber_gem', index: 0 },
+            { item: 'betterendforge:crystal_shards', index: 2 },
+            { item: 'betterendforge:crystal_shards', index: 4 },
+            { item: 'betterendforge:crystal_shards', index: 6 }
+        ]
+    }).id('betterendforge:crystalite_leggings')
     //Boots
-    event.custom(
-        {
-            "type": "betterendforge:infusion",
-            "input":
-            {
-                "item": "minecraft:netherite_boots"
-            },
-            "output": "betterendforge:crystalite_boots",
-            "time": 150,
-            "catalysts": [
-                {
-                    "item": "betterendforge:amber_gem",
-                    "index": 0
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 2
-                },
-                {
-                    "item": "betterendforge:crystal_shards",
-                    "index": 6
-                }
-            ]
-        }
-    ).id('betterendforge:')
-
-
-
+    event.custom({
+        type: 'betterendforge:infusion',
+        input: { item: 'minecraft:netherite_boots' },
+        output: 'betterendforge:crystalite_boots',
+        time: 150,
+        catalysts: [
+            { item: 'betterendforge:amber_gem', index: 0 },
+            { item: 'betterendforge:crystal_shards', index: 2 },
+            { item: 'betterendforge:crystal_shards', index: 6 }
+        ]
+    }).id('betterendforge:crystalite_boots')
 
 })
